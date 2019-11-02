0c7cdc8a03952954b264d0389d10d96ece630d58
tonychang <tony@csp-inc.org>
2019-11-02 13:23:26 -0700

    added google-chrome browser environment

--- run_rstudio.sh
+++ run_rstudio.sh
@@ -4,8 +4,9 @@ docker run -it --rm \
   -v $(pwd):/contents \
   -w /contents \
   -p 8787:8787 \
-  -p 3838:3838 \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -e BROWSER='google-chrome' \
   --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
+  --device="/dev/dri"\
   apis/rayshader:latest 
